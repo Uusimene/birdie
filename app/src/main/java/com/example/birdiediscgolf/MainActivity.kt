29681ab18cbbb422a29f756a290dc623a62572ce
Juuso Toivonen <juuso.toivonen@tuni.fi>
2020-01-31 21:52:28 +0200

    import export navigation bar menu added

--- app/src/main/java/com/example/birdiediscgolf/MainActivity.kt
+++ app/src/main/java/com/example/birdiediscgolf/MainActivity.kt
@@ -3,13 +3,40 @@ package com.example.birdiediscgolf
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.import_export_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.importData -> {
+                Toast.makeText(applicationContext, "Import Data Pressed", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.exportData -> {
+                Toast.makeText(applicationContext, "Export Data Pressed", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> {
+                Toast.makeText(applicationContext, "How do I shot web", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     fun startNewGame(view: View){
